67ca261fdbd42ca2622d1aab218a932b64aeeb0c
Adriana Collaguazo Jaramillo <30510475+acollaguazo@users.noreply.github.com>
2022-12-18 19:56:17 -0500

    Add files via upload
    ejercicios realizados

--- final_project/index.js
+++ final_project/index.js
@@ -1,17 +1,37 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+import express, { json } from 'express';
+import { verify } from 'jsonwebtoken';
+import session from 'express-session';
+import { authenticated as customer_routes } from './router/auth_users.js';
+import { general as genl_routes } from './router/general.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if (req.session.authorization) {
+    // get the session token
+    token = req.session.authorization['accessToken'];
+    // verify the token
+    verify(token, "access", (err, user) => {
+        if (!err) {
+            // cache the user object in the request
+            req.user = user;
+            next();
+        }
+        // the token has expired or is invalid
+        else {
+            return res.status(403).json({message: "User not authenticated"})
+        }
+    });
+}
+// there has not been a log in even in this session
+else {
+    return res.status(403).json({message: "User not logged in"})
+}
 });
  
 const PORT =5000;
